3ca72e74142c9533591433fde0c3e310c40f0224
Keshav <keshavgogia57@gmail.com>
2024-09-15 15:41:05 +0530

    Update Messages.jsx

--- dashboard/src/components/Messages.jsx
+++ dashboard/src/components/Messages.jsx
@@ -11,7 +11,7 @@ const Messages = () => {
     const fetchMessages = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:8000/api/v1/message/getall",
+          `${process.env.REACT_APP_BACKEND_URL}/api/v1/message/getall`,
           { withCredentials: true }
         );
         setMessages(data.messages);
