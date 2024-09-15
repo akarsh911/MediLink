398507cf21929a6e059059cfae6761c56f410782
Keshav <keshavgogia57@gmail.com>
2024-09-15 15:41:13 +0530

    -ur

--- dashboard/src/components/Dashboard.jsx
+++ dashboard/src/components/Dashboard.jsx
@@ -13,7 +13,7 @@ const Dashboard = () => {
     const fetchAppointments = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:8000/api/v1/appointment/getall",
+          `${process.env.REACT_APP_BACKEND_URL}/api/v1/appointment/getall`,
           { withCredentials: true }
         );
         setAppointments(data.appointments);
@@ -27,7 +27,7 @@ const Dashboard = () => {
   const handleUpdateStatus = async (appointmentId, status) => {
     try {
       const { data } = await axios.put(
-        `http://localhost:8000/api/v1/appointment/update/${appointmentId}`,
+        `${process.env.REACT_APP_BACKEND_URL}/api/v1/appointment/update/${appointmentId}`,
         { status },
         { withCredentials: true }
       );
--- dashboard/src/components/Doctors.jsx
+++ dashboard/src/components/Doctors.jsx
@@ -11,7 +11,7 @@ const Doctors = () => {
     const fetchDoctors = async () => {
       try {
         const { data } = await axios.get(
-          "http://localhost:8000/api/v1/user/doctors",
+          `${process.env.REACT_APP_BACKEND_URL}/api/v1/user/doctors`,
           { withCredentials: true }
         );
         setDoctors(data.doctors);
